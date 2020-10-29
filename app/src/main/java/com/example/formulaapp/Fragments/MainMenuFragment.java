7a6ed9d917b2d618b7b28871c63fd9d3c1715838
mmh-dev <murod.hodjaev@gmail.com>
2020-10-30 04:37:03 +0600

    register-fragments part 1

---
+++ app/src/main/java/com/example/formulaapp/Fragments/MainMenuFragment.java
@@ -0,0 +1,26 @@
+package com.example.formulaapp.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.formulaapp.R;
+
+
+public class MainMenuFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        Toast.makeText(getContext(), "Main", Toast.LENGTH_SHORT).show();
+        return view;
+    }
+}
