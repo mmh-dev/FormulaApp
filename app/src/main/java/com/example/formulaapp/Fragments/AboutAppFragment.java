fa783bcf82bd2e6a9f87a9c1592af11f76fc41a1
mmh-dev <murod.hodjaev@gmail.com>
2020-11-13 00:21:02 +0600

    about app fragment done

--- app/src/main/java/com/example/formulaapp/Fragments/AboutAppFragment.java
+++ app/src/main/java/com/example/formulaapp/Fragments/AboutAppFragment.java
@@ -1,5 +1,7 @@
 package com.example.formulaapp.Fragments;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,18 +9,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.formulaapp.BuildConfig;
 import com.example.formulaapp.R;
 
 
 public class AboutAppFragment extends Fragment {
+
+    TextView app_version, os_version, device_model, dev_email;
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about_app, container, false);
+        View view = inflater.inflate(R.layout.fragment_about_app, container, false);
+
+        app_version = view.findViewById(R.id.app_version);
+        os_version = view.findViewById(R.id.os_version);
+        device_model = view.findViewById(R.id.device_model);
+        dev_email = view.findViewById(R.id.dev_email);
+
+        app_version.setText(BuildConfig.VERSION_NAME);
+        os_version.setText(String.valueOf(android.os.Build.VERSION.RELEASE));
+        device_model.setText(android.os.Build.MODEL);
+
+        dev_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:"));
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"murod.hodjaev@gmail.com"});
+                if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+        return view;
     }
 }
