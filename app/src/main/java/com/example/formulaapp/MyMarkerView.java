3d699bf1485fb09171be847ffba3f1aef8d71cb9
mmh-dev <murod.hodjaev@gmail.com>
2020-11-19 12:10:24 +0600

    rating fragment 1

---
+++ app/src/main/java/com/example/formulaapp/MyMarkerView.java
@@ -0,0 +1,44 @@
+
+package com.example.formulaapp;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.CandleEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+import com.github.mikephil.charting.utils.Utils;
+
+/**
+ * Custom implementation of the MarkerView.
+ *
+ * @author Philipp Jahoda
+ */
+@SuppressLint("ViewConstructor")
+public class MyMarkerView extends MarkerView {
+
+    private final TextView tvContent;
+
+    public MyMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+
+        tvContent = findViewById(R.id.tvContent);
+    }
+
+    // runs every time the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+        tvContent.setText(Utils.formatNumber(e.getY(), 0, true));
+        super.refreshContent(e, highlight);
+    }
+
+    @Override
+    public MPPointF getOffset() {
+        return new MPPointF(-(getWidth() / 2), -getHeight());
+    }
+}
